5c1dac56b9881ad0da2d6434273a692b4da05705
Its EmmaNames <emmanuelortiz529@gmail.com>
2026-05-25 12:23:57 -0600

    Delete PF03 directory

--- PF03/Sign_Extend.v
+++
@@ -1,7 +0,0 @@
-module SignExtend (
-    input [15:0] in,
-    output [31:0] out
-);
-    // Replica el bit de signo (el bit 15) 16 veces hacia la izquierda
-    assign out = {{16{in[15]}}, in};
-endmodule
